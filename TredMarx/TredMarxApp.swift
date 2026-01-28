e087dd8163034cd47c72681991517feb9adac8e0
Ed Singleton <ed@singletoned.com>
2026-01-28 19:52:42 +0000

    Initial Commit

---
+++ TredMarx/TredMarxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TredMarxApp.swift
+//  TredMarx
+//
+//  Created by Ed Singleton on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TredMarxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
